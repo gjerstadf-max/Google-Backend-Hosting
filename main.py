7d81ec8dd418d83f946c361689b70ad00a2a9923
gjerstadf-max <gjerstadf.gmail.com>
2025-12-19 15:08:53 -0800

    small changes to main.py file

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import os
 
 app = FastAPI()
 
+import os
+
+tavily_key = os.environ.get("TAVILY_API_KEY")
+openai_key = os.environ.get("OPENAI_API_KEY")
+
 @app.get("/")
 def health():
     return {"status": "If you see this, the cloud is finally working!"}
